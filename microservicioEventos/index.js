22dd28ea278aa2a04c679f86a1f7e13dc1dae574
Albaacg <alba.cunado@opendeusto.es>
2024-11-11 16:40:23 +0100

    datos

--- microservicioEventos/index.js
+++ microservicioEventos/index.js
@@ -1,6 +1,38 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3000;
+const axios = require('axios');
+const port = 4000;
+const apiKey = process.env.API_KEY;
+
+app.get('/eventos/all', async (req, res) => {
+    try {
+        const url = `http://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}`;
+
+        console.log(apiKey)
+
+        const response = await axios.get(url);
+        const data = response.data;
+
+        const eventos = data._embedded.events.map(event => {
+            return {
+                name: event.name,
+                date: event.dates.start.localDate,
+                time: event.dates.start.localTime,
+                city: event._embedded.venues[0].city.name,
+                price: event.priceRanges[0].min + ' - ' + event.priceRanges[0].max
+            };
+        })
+        
+        res.json(eventos);
+
+        console.log('Events obtained successfully');
+
+    } catch (error) {
+        console.error('Error obtaining the events: ' + error);
+        res.status(500).send('Error obtaining the events');
+    }
+})
 
 app.listen(port, () => {
     console.log('Servior escuchando en ' + port)
